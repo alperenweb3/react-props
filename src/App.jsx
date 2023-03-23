793dfae6ba4a5f16597d6f0aace90f199f945596
Alperen Özkan <alperenweb3@gmail.com>
2023-03-23 14:14:03 +0100

    button component updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import './App.css';
 //import Props from "./components/example1/App";
-import Props from "./components/example2/App";
-//import Button from "./components/example3/Button";
+//import Props from "./components/example2/App";
+import Button from "./components/example3/Button";
 //import Props from "./components/example4/App";
 //import Props from "./components/example4/App";
 //import Props from "./components/example5/Router";
 
-
+const onClicked = () => {
+  alert("Hello I am onClicked")
+}
 
 
 function App() {
   return (
     <div className="App">
       <h1>React Props</h1>
-      <Props />
+      <Button bgcolor="green" color="white" children="Buy Now" size="medium" onClick={onClicked} />
     </div>
   );
 }
--- src/components/example3/Button.js
+++ src/components/example3/Button.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
 
 function Button(props) {
   const styles = {
     backgroundColor: props.bgcolor,
-    fontSize: props.size === 'large' ? '24px' : '16px',
-    padding: props.size === 'large' ? '12px 24px' : '8px 16px',
-    color: props.color
+    fontSize:
+      props.size === "large"
+        ? "24px"
+        : props.size === "medium"
+        ? "18px"
+        : "16px",
+    padding:
+      props.size === "large"
+        ? "12px 24px"
+        : props.size === "medium"
+        ? "10px 18px"
+        : "8px 16px",
+    color: props.color,
   };
+
+  console.log(props);
 
   return (
     <button style={styles} onClick={props.onClick}>
